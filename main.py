3b84af062f39c38993d4a703a1286b7bed1c27c0
allan <drmundo920@gmail.com>
2024-04-08 11:48:30 -0300

    crud cliente_promoções e cliente_avaliações

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from datetime import date
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 import psycopg2
 from psycopg2 import sql
 
-## comando rodar uvicode uvicorn main:app --reload --port 8080
+
+# comando rodar uvicode uvicorn main:app --reload --port 8080
+
 # Definindo modelos Pydantic para as tabelas
 class Cliente(BaseModel):
     nome: str
@@ -23,6 +27,19 @@ class Promocao(BaseModel):
     nome: str
     descricao: Optional[str] = None
     desconto: float
+
+
+class Avaliacao(BaseModel):
+    id_cliente: int
+    id_filme: int
+    nota: int
+    comentario: Optional[str] = None
+    data_avaliacao: date
+
+
+class ClientePromocao(BaseModel):
+    id_cliente: int
+    id_promocao: int
 
 
 # Configuração do banco de dados
@@ -158,3 +175,98 @@ def delete_promocao(promocao_id: int):
         conn.commit()
         return {"message": f"Promoção com id {promocao_id} deletada com sucesso"}
 
+
+# Rotas CRUD para a tabela de avaliacoes geradas por chaves estrageiras de cliente e filme
+@app.post("/avaliacoes/")
+def create_avaliacao(avaliacao: Avaliacao):
+    query = sql.SQL("INSERT INTO cinema.avaliacoes (id_cliente, id_filme, nota, comentario, data_avaliacao) VALUES ("
+                    "%s, %s, %s, %s, %s)")
+    data = (avaliacao.id_cliente, avaliacao.id_filme, avaliacao.nota, avaliacao.comentario, avaliacao.data_avaliacao)
+    with connect_to_db() as conn, conn.cursor() as cur:
+        cur.execute(query, data)
+        conn.commit()
+        return {"message": "Avaliação criada com sucesso"}
+
+
+@app.get("/avaliacoes/")
+def read_avaliacoes():
+    query = sql.SQL("SELECT * FROM cinema.avaliacoes")
+    with connect_to_db() as conn, conn.cursor() as cur:
+        cur.execute(query)
+        return [{"id_avaliacao": row[0], "id_cliente": row[1], "id_filme": row[2], "nota": row[3], "comentario": row[4],
+                 "data_avaliacao": row[5]} for row in cur.fetchall()]
+
+
+@app.get("/avaliacoes/{avaliacao_id}")
+def read_avaliacao(avaliacao_id: int):
+    query = sql.SQL("SELECT * FROM cinema.avaliacoes WHERE id_avaliacao = %s")
+    with connect_to_db() as conn, conn.cursor() as cur:
+        cur.execute(query, (avaliacao_id,))
+        row = cur.fetchone()
+        if row:
+            return {"id_avaliacao": row[0], "id_cliente": row[1], "id_filme": row[2], "nota": row[3],
+                    "comentario": row[4], "data_avaliacao": row[5]}
+        else:
+            raise HTTPException(status_code=404, detail="Avaliação não encontrada")
+
+
+@app.put("/avaliacoes/{avaliacao_id}")
+def update_avaliacao(avaliacao_id: int, avaliacao: Avaliacao):
+    query = sql.SQL("UPDATE cinema.avaliacoes SET id_cliente = %s, id_filme = %s, nota = %s, comentario = %s, "
+                    "data_avaliacao = %s WHERE id_avaliacao = %s")
+    data = (avaliacao.id_cliente, avaliacao.id_filme, avaliacao.nota, avaliacao.comentario, avaliacao.data_avaliacao,
+            avaliacao_id)
+    with connect_to_db() as conn, conn.cursor() as cur:
+        cur.execute(query, data)
+        conn.commit()
+        return {"message": f"Avaliação com id {avaliacao_id} atualizada com sucesso"}
+
+
+# Rotas CRUD para a tabela de clientes_promocoes
+@app.post("/clientes_promocoes/")
+def create_cliente_promocao(cliente_promocao: ClientePromocao):
+    query = sql.SQL("INSERT INTO cinema.clientes_promocoes (id_cliente, id_promocao) VALUES (%s, %s)")
+    data = (cliente_promocao.id_cliente, cliente_promocao.id_promocao)
+    with connect_to_db() as conn, conn.cursor() as cur:
+        cur.execute(query, data)
+        conn.commit()
+        return {"message": "Cliente participação em promoção adicionada com sucesso"}
+
+
+@app.get("/clientes_promocoes/")
+def read_clientes_promocoes():
+    query = sql.SQL("SELECT * FROM cinema.clientes_promocoes")
+    with connect_to_db() as conn, conn.cursor() as cur:
+        cur.execute(query)
+        return [{"id_cliente": row[0], "id_promocao": row[1]} for row in cur.fetchall()]
+
+@app.get("/clientes_promocoes/{cliente_id}/{promocao_id}")
+def read_cliente_promocao(cliente_id: int, promocao_id: int):
+    query = sql.SQL("SELECT * FROM cinema.clientes_promocoes WHERE id_cliente = %s AND id_promocao = %s")
+    with connect_to_db() as conn, conn.cursor() as cur:
+        cur.execute(query, (cliente_id, promocao_id))
+        row = cur.fetchone()
+        if row:
+            return {"id_cliente": row[0], "id_promocao": row[1]}
+        else:
+            raise HTTPException(status_code=404, detail="Participação do cliente na promoção não encontrada")
+
+
+@app.put("/clientes_promocoes/{cliente_id}/{promocao_id}")
+def update_cliente_promocao(cliente_id: int, promocao_id: int, cliente_promocao: ClientePromocao):
+    query = sql.SQL("UPDATE cinema.clientes_promocoes SET id_cliente = %s, id_promocao = %s WHERE id_cliente = %s AND "
+                    "id_promocao = %s")
+    data = (cliente_promocao.id_cliente, cliente_promocao.id_promocao, cliente_id, promocao_id)
+    with connect_to_db() as conn, conn.cursor() as cur:
+        cur.execute(query, data)
+        conn.commit()
+        return {"message": f"Participação do cliente na promoção atualizada com sucesso"}
+
+
+@app.delete("/clientes_promocoes/{cliente_id}/{promocao_id}")
+def delete_cliente_promocao(cliente_id: int, promocao_id: int):
+    query = sql.SQL("DELETE FROM cinema.clientes_promocoes WHERE id_cliente = %s AND id_promocao = %s")
+    with connect_to_db() as conn, conn.cursor() as cur:
+        cur.execute(query, (cliente_id, promocao_id))
+        conn.commit()
+        return {"message": f"Participação do cliente na promoção deletada com sucesso"}
